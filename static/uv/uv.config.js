d98badd3c9e80b73e0ea96b6055851f656c90dad
arcticmath <arcticmath@gmail.com>
2026-02-25 16:58:51 -0600

    Add bare prefix to UV configuration

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
