2c596e1b857ccdf7cea3f8170a4837e4a8824091
arcticmath <arcticmath@gmail.com>
2026-02-25 23:49:56 -0600

    Update register.js

--- static/assets/js/register.js
+++ static/assets/js/register.js
@@ -1,8 +1,7 @@
 const stockSW = "/uv/sw.js";
 const swAllowedHostnames = ["localhost", "127.0.0.1"];
-const wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+const wispUrl = "wss://ixlmathenglish.wikidelia.net/";
 const connection = new BareMux.BareMuxConnection("/baremux/worker.js");
-
 async function registerSW() {
   if (!navigator.serviceWorker) {
     if (
@@ -10,10 +9,8 @@ async function registerSW() {
       !swAllowedHostnames.includes(location.hostname)
     )
       throw new Error("Service workers cannot be registered without https.");
-
     throw new Error("Your browser doesn't support service workers.");
   }
-
   await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
   await window.navigator.serviceWorker.register("/sw.js", {
     scope: '/service/',
@@ -22,5 +19,4 @@ async function registerSW() {
     scope: '/assignments/',
   });
 }
-
 registerSW();
