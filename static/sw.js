c750a965b1459b8c416460787538c00a6a87ca45
arcticmath <arcticmath@gmail.com>
2026-02-25 21:45:27 -0600

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -2,16 +2,8 @@ importScripts('/uv/uv.bundle.js');
 importScripts('/uv/uv.config.js');
 importScripts('/uv/uv.sw.js');
 
-const uv = new UVServiceWorker();
-
-async function handleRequest(event) {
-    if (uv.route(event)) {
-        return await uv.fetch(event);
-    }
-    
-    return await fetch(event.request)
-}
+const sw = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(handleRequest(event));
+    event.respondWith(sw.fetch(event));
 });
