6e0fd8a82ebf38dda490fdaf6710510e6d59a45b
mathpunch Official <coutsozzy09@gmail.com>
2026-02-26 11:10:51 -0600

    Update main.js

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,8 +1,22 @@
+// Preconnect to CDN
+var preconnect = document.createElement("link");
+preconnect.rel = "preconnect";
+preconnect.href = "https://cdn.jsdelivr.net";
+document.head.appendChild(preconnect);
+
+// Preload Bootstrap icons CSS
+var preload = document.createElement("link");
+preload.rel = "preload";
+preload.as = "style";
+preload.href = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css";
+document.head.appendChild(preload);
+
 function loadScript(src, callback) {
   var script = document.createElement("script");
   script.type = "text/javascript";
   script.src = src;
-  script.onload = callback;
+  script.defer = true;
+  if (callback) script.onload = callback;
   document.head.appendChild(script);
 }
 
@@ -11,10 +25,9 @@ function loadCSS(href, callback) {
   link.rel = "stylesheet";
   link.href = href;
   var supportsOnLoad = "onload" in link;
-  if(supportsOnLoad) {
+  if (supportsOnLoad) {
     link.onload = callback;
-  }
-  else {
+  } else {
     setTimeout(function() {
       callback();
     }, 1000);
@@ -22,43 +35,50 @@ function loadCSS(href, callback) {
   document.head.appendChild(link);
 }
 
-loadCSS('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css', function() {
-  console.log('[✔️] Bootstrap icons');
-});
-if(window.location.pathname === "/index.html" || window.location.pathname === "/") {
-  console.log("[✔️] Index.html");
-  const options = ["Right-Click to access more features", "Set a custom background in settings.", "Tab Cloaking is highly recommended", "About:Blank Cloak is highly recommended", "This site was originally created as a joke", "Hosted on the best hardware!", "Do not share this link with anyone.", "Join our Discord for more cool things!", "Join our Discord for more cool things!", "© Copyright 2024 Doge Network. All Rights Reserved.", "Customize Doge by going to Settings > Themes", "Want more links? Our Discord provides tons of links!", "Want more links? Our Discord provides tons of links!", "Want more links? Our Discord provides tons of links!", "Hi, fowntain here!", ];
+loadCSS('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css', function() {});
+
+if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
+  const options = ["Right-Click to access more features", "Set a custom background in settings.", "Tab Cloaking is highly recommended", "About:Blank Cloak is highly recommended", "This site was originally created for fun and educational purposes."];
 
   function getRandomOption() {
     const randomNumber = Math.floor(Math.random() * options.length);
     return options[randomNumber];
   }
 
+  // Cache DOM elements once
+  const placeholder = document.getElementById("placeholder");
+  const bar = document.querySelector(".browse-input");
+  const search = document.getElementById("search");
+
   function setRandomPlaceholder() {
-    const placeholder = document.getElementById("placeholder");
     placeholder.textContent = getRandomOption();
   }
+
   setRandomPlaceholder();
-  var bar = document.querySelector(".browse-input");
-  var search = document.getElementById("search");
+
   bar.addEventListener("focus", () => {
     search.style.marginLeft = "-367px";
   });
+
   bar.addEventListener("blur", () => {
     search.style.marginLeft = "-150px";
   });
 }
+
 window.addEventListener("load", function() {
-  loadScript("/worker.js");
-  if(window.location.pathname === "/index.html" || window.location.pathname === "/") {
-    if(window.innerWidth < 676) {
-      location.href = "/mobile.html";
-    }
+  // Register service worker for asset caching
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js");
   }
-  if(window.location.pathname === '/loading.html') {
-    if(window.innerWidth < 676) {
-      var rm = document.querySelector('.themesExcluded');
-      rm.style.display = 'none';
-    }
+
+  loadScript("/worker.js");
+
+  if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
+    location.href = "/mobile.html";
+  }
+
+  if (window.location.pathname === '/loading.html') {
+    var rm = document.querySelector('.themesExcluded');
+    if (rm) rm.style.display = 'none';
   }
 });
